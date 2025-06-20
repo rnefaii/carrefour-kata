ff11a6aec3691206ef6566425a5a88acb2fca905
rnefaii <nefai.rahma@gmail.com>
2025-06-20 10:32:11 +0100

    remove delete component

--- leasingCar/src/app/features/customer/customer-routing.module.ts
+++ leasingCar/src/app/features/customer/customer-routing.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CustomerComponent } from './customer.component';
-import {CustomerDeleteComponent} from "./pages/customer-delete/customer-delete.component";
 import {CustomerEditComponent} from "./pages/customer-edit/customer-edit.component";
 import {CustomerCreateComponent} from "./pages/customer-create/customer-create.component";
 import {CustomerListComponent} from "./pages/customer-list/customer-list.component";
@@ -9,8 +7,7 @@ import {CustomerListComponent} from "./pages/customer-list/customer-list.compone
 const routes: Routes = [
   { path: '', component: CustomerListComponent },
   { path: 'create', component: CustomerCreateComponent },
-  { path: 'edit/:id', component: CustomerEditComponent },
-  { path: 'detail/:id', component: CustomerDeleteComponent },
+  { path: 'edit/:id', component: CustomerEditComponent }
 ];
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- leasingCar/src/app/features/customer/customer.module.ts
+++ leasingCar/src/app/features/customer/customer.module.ts
@@ -6,7 +6,6 @@ import { CustomerComponent } from './customer.component';
 import { CustomerListComponent } from './pages/customer-list/customer-list.component';
 import { CustomerCreateComponent } from './pages/customer-create/customer-create.component';
 import { CustomerEditComponent } from './pages/customer-edit/customer-edit.component';
-import { CustomerDeleteComponent } from './pages/customer-delete/customer-delete.component';
 import {SharedModule} from "../../shared/shared.module";
 
 
@@ -16,7 +15,6 @@ import {SharedModule} from "../../shared/shared.module";
     CustomerListComponent,
     CustomerCreateComponent,
     CustomerEditComponent,
-    CustomerDeleteComponent
   ],
   imports: [
     CommonModule,
